653054808f9d937ba5b09d2c732703bd192b970b
yuhsiangl <aalex910606@gmail.com>
2025-03-04 02:20:08 +0800

    feat: credit_card.rb done

--- credit_card.rb
+++ credit_card.rb
@@ -2,21 +2,31 @@
 
 require_relative './luhn_validator'
 require 'json'
+require 'digest'
 
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
+  include LuhnValidator
 
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
   def to_json
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      number: @number,
+      expiration_date: @expiration_date,
+      owner: @owner,
+      credit_network: @credit_network
     }.to_json
   end
 
@@ -28,6 +38,8 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    data = JSON.parse(card_s)
+    new(data['number'], data['expiration_date'],data['owner'],data['credit_network'])
   end
 
   # return a hash of the serialized credit card object
@@ -36,6 +48,7 @@ class CreditCard
     #   - Produce a hash (using default hash method) of the credit card's
     #     serialized contents.
     #   - Credit cards with identical information should produce the same hash
+    to_json.hash
   end
 
   # return a cryptographically secure hash
@@ -43,5 +56,14 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    Digest::SHA256.hexdigest(to_json)
   end
 end
+
+# test code
+# number = '4539075978941247' # visa card test
+# card = CreditCard.new(number, nil, nil, nil)
+# puts card
+# puts CreditCard.from_s(card.to_s)
+# puts card.hash
+# puts card.hash_secure
